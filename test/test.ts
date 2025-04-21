e12a7934cec8c8c6808bb92522c23e68212d9dde
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2025-04-22 00:27:24 +0200

    Fixed outdated function arguments in tests
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

--- test/test.ts
+++ test/test.ts
@@ -5,27 +5,27 @@ import { describe, expect, test } from "vitest";
 
 describe("Game", () => {
   test("can be created with columns and rows", () => {
-    let game = new Game(4, 3, null);
+    let game = new Game(4, 3);
     expect(game.rows).toBe(3);
     expect(game.columns).toBe(4);
   });
   describe("is_empty()", () => {
     test("returns false for walls", () => {
-      let game = new Game(5, 5, null);
+      let game = new Game(5, 5);
       expect(game.is_empty(0, 0)).toBe(false);
     });
     test("Returns true for empty tiles", () => {
-      let game = new Game(5, 5, null);
+      let game = new Game(5, 5);
       expect(game.is_empty(1, 1)).toBe(true);
     });
     test("Returns false for goal", () => {
-      let game = new Game(5, 5, null);
+      let game = new Game(5, 5);
       let c = game.goal.c;
       let r = game.goal.r;
       expect(game.is_empty(c, r)).toBe(false);
     });
     test("Returns false for spawn", () => {
-      let game = new Game(5, 5, null);
+      let game = new Game(5, 5);
       let c = game.spawn.c;
       let r = game.spawn.r;
       expect(game.is_empty(c, r)).toBe(false);
@@ -33,7 +33,7 @@ describe("Game", () => {
   });
   describe("is_outside()", () => {
     test("returns false for positions inside", () => {
-      let game = new Game(10, 20, null);
+      let game = new Game(10, 20);
       expect(game.is_outside(0, 0)).toBe(false);
       expect(game.is_outside(1, 1)).toBe(false);
       expect(game.is_outside(9, 19)).toBe(false);
@@ -41,7 +41,7 @@ describe("Game", () => {
       expect(game.is_outside(1, 19)).toBe(false);
     });
     test("returns true for positions outside", () => {
-      let game = new Game(10, 20, null);
+      let game = new Game(10, 20);
       expect(game.is_outside(-1, 0)).toBe(true);
       expect(game.is_outside(0, -1)).toBe(true);
       expect(game.is_outside(10, 20)).toBe(true);
@@ -51,7 +51,7 @@ describe("Game", () => {
   });
   describe("place_tower()", () => {
     test("places a tower", () => {
-      let game = new Game(5, 5, null);
+      let game = new Game(5, 5);
       expect(game.towers.length).toBe(0);
       let p = game.find_empty_not_path();
       game.place_tower(p.c, p.r, "Gun tower");
@@ -60,7 +60,7 @@ describe("Game", () => {
   });
   describe("grid_click()", () => {
     test("places a tower", () => {
-      let game = new Game(5, 5, null);
+      let game = new Game(5, 5);
       expect(game.towers.length).toBe(0);
       let p = game.find_empty_not_path();
       expect(p).not.toBe(null);
@@ -68,7 +68,7 @@ describe("Game", () => {
       expect(game.towers.length).toBe(1);
     });
     test("doesn't place a tower when dead", () => {
-      let game = new Game(5, 5, null);
+      let game = new Game(5, 5);
       expect(game.towers.length).toBe(0);
       let p = game.find_empty_not_path();
       game.lives = 0;
@@ -76,7 +76,7 @@ describe("Game", () => {
       expect(game.towers.length).toBe(0);
     });
     test("doesn't block the path", () => {
-      let game = new Game(3, 3, null);
+      let game = new Game(3, 3);
       expect(game.towers.length).toBe(0);
       let row = game.spawn.r;
       game.grid_click(1, row, "Gun tower");
@@ -84,13 +84,13 @@ describe("Game", () => {
       expect(game.is_empty(1, row)).toBe(true);
     });
     test("doesn't place a tower on a wall", () => {
-      let game = new Game(5, 5, null);
+      let game = new Game(5, 5);
       expect(game.towers.length).toBe(0);
       game.grid_click(0, 0, "Gun tower");
       expect(game.towers.length).toBe(0);
     });
     test("doesn't place a tower on a tower", () => {
-      let game = new Game(5, 5, null);
+      let game = new Game(5, 5);
       expect(game.towers.length).toBe(0);
       let p = game.find_empty_not_path();
       game.grid_click(p.c, p.r, "Gun tower");
@@ -99,7 +99,7 @@ describe("Game", () => {
       expect(game.towers.length).toBe(1);
     });
     test("doesn't place a tower when you have no money", () => {
-      let game = new Game(5, 5, null);
+      let game = new Game(5, 5);
       game.money = 0;
       expect(game.towers.length).toBe(0);
       let row = game.is_path(1, 1) ? 2 : 1;
@@ -109,23 +109,23 @@ describe("Game", () => {
   });
   describe("is_empty()", () => {
     test("Game.is_empty() returns true by default", () => {
-      let game = new Game(4, 3, null);
+      let game = new Game(4, 3);
       expect(game.is_empty(1, 1)).toBe(true);
     });
 
     test("Game.is_empty() returns false for wall", () => {
-      let game = new Game(10, 10, null);
+      let game = new Game(10, 10);
       expect(game.is_empty(0, 0)).toBe(false);
       expect(game.tiles[0][0], "wall");
     });
   });
   describe("can_afford()", () => {
     test("returns true at beginning of game", () => {
-      let game = new Game(4, 3, null);
+      let game = new Game(4, 3);
       expect(game.can_afford("Gun tower")).toBe(true);
     });
     test("returns false when you have no money", () => {
-      let game = new Game(4, 3, null);
+      let game = new Game(4, 3);
       game.money = 0;
       expect(game.can_afford("Gun tower")).toBe(false);
     });
